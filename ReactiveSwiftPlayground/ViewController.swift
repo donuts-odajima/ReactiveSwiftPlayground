8607436fa3576b1e3e3400d1ee7d515d5a198a23
Naoki Odajima <odajima.naoki@donuts.ne.jp>
2020-10-06 16:30:34 +0900

    isSelectedのときにフォント変更

--- ReactiveSwiftPlayground/ViewController.swift
+++ ReactiveSwiftPlayground/ViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Naoki Odajima. All rights reserved.
 //
 
-//import ReactiveCocoa
+import ReactiveCocoa
 import ReactiveSwift
 import UIKit
 
@@ -20,11 +20,6 @@ import UIKit
 final class ViewController: UIViewController {
     @IBOutlet weak private var incrementButton: UIButton!
     @IBOutlet weak private var decrementButton: UIButton!
-    @IBOutlet weak private var countLabel: UILabel!
-
-    // 型と初期値でインスタンスを生成
-    // MutablePropertyの定義にもあるが、スレッドセーフ
-    private let count = MutableProperty<Int>(-3)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,34 +31,13 @@ final class ViewController: UIViewController {
         self.incrementButton.addTarget(self, action: #selector(self.incrementButtonPressed(_:)), for: .primaryActionTriggered)
         // decrementButton が押された時
         self.decrementButton.addTarget(self, action: #selector(self.decrementButtonPressed(_:)), for: .primaryActionTriggered)
-        // count の変更を監視する
-        // producer でバインドすると MutableProperty の初期値もストリームに流れる
-        self.count.producer
-            .start(on: UIScheduler()) // メインスレッドで監視
-            .startWithValues { [weak self] count in
-                // countLabel のテキストを変更
-                self?.countLabel.text = String(count)
-            }
-        // signal でバインドすると初期値は流れない
-//        self.count.signal
-//            .observe(on: UIScheduler())
-//            .observeValues { [weak self] count in
-//               self?.countLabel.text = String(count)
-//           }
     }
 
-    /// count を +1 する
     @objc private func incrementButtonPressed(_ sender: UIButton) {
-        // .value で普通の変数と同じように set get できる
-        // 後述する modify を内部で使用しているためスレッドセーフ
-        self.count.value += 1
     }
-
-    /// count を -1 する
+    
     @objc private func decrementButtonPressed(_ sender: UIButton) {
-        // 自分の値を利用して変更する時に便利
-        // スレッドセーフ
-        self.count.modify { $0 = $0 - 1 }
+        self.incrementButton.isSelected = !self.incrementButton.isSelected
     }
 }
 
